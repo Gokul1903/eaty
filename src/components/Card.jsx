3db6739245e8900bb03285c29995e0ad37098071
Gokul1903 <rajgokul436@gmail.com>
2025-04-11 17:35:03 +0530

    card changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL;
+const IMG_URI=import.meta.env.VITE_CLOUDINARY_BASE_URL
 const Card = ({ title, price,id,availability,image}) => {
     return (
       <div className="card my-3 "  style={{ width: "100%",border:"none" }}>
         <img 
           className="card-img-top rounded-top-4" 
-          src={`${API_URL}${image}`} 
+          src={`${IMG_URI}/eaty-images/${image.split('/').pop()}`}
           alt={"image"} 
           style={{ height: "200px", objectFit: "fill" }} 
           loading="lazy"
         />
+        {console.log(`${IMG_URI}/eaty-images/${image.split('/').pop()}`)}
         <div className="card-body nav-bg text-white rounded-bottom-4 ">
         
           <h5 className="card-title text-center py-2 text-capitalize">{title} </h5>
