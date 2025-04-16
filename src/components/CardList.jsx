91d026a7cfc90f1072b714b9dc7163f91b7a6ae6
Gokul1903 <rajgokul436@gmail.com>
2025-04-16 17:07:32 +0530

    added animations

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useContext } from "react";
 import Card from "./Card";
 import { GlobalContext } from "../context/GlobalContext";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const CardList = () => {
   const { products, fetchProduct } = useContext(GlobalContext);
 
   useEffect(() => {
     fetchProduct();
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: false,
+  mirror: true,// animation happens only once while scrolling down
+    });
   }, []);
+  
 
   // Show full-screen centered spinner when loading
   if (products.length === 0) {
@@ -27,9 +37,17 @@ const CardList = () => {
   return (
     <section className="py-5">
       <div className="container">
-        <div className="row">
+        <div className="row card-hover-group">
           {products.map((card) => (
-            <div className="col-md-3" key={card._id}>
+            <div
+            className="col-md-3 card-hover-wrapper"
+            key={card._id}
+            data-aos={"fade-up"}
+            data-aos-offset="100"
+            data-aos-delay="50"
+            data-aos-duration="800"
+          >
+            <div className="card-hover-effect">
               <Card
                 title={card.name}
                 price={card.price}
@@ -37,7 +55,7 @@ const CardList = () => {
                 shopid={card.ShopId}
                 availability={card.availability}
                 image={card.image}
-              />
+              /></div>
             </div>
           ))}
         </div>
